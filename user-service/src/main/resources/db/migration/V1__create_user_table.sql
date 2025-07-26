871575bf6b3b5dd7a8e88b22c2f4e008bc1faa86
dhiraj.shah04@gmail.com <dhiraj.shah04@gmail.com>
2025-07-26 14:31:04 +0545

    add user-service

---
+++ user-service/src/main/resources/db/migration/V1__create_user_table.sql
@@ -0,0 +1,8 @@
+CREATE TABLE users
+(
+    id       SERIAL PRIMARY KEY,
+    username VARCHAR(100) NOT NULL UNIQUE,
+    email    VARCHAR(150) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL,
+    role     VARCHAR(50)  NOT NULL
+);
